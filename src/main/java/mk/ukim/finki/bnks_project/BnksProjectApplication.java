ae2866a3fb0e09d5b1ce0260189ff0d872635c07
Душица Јанковиќ <dusica.jankovik@gmail.com>
2020-07-02 23:26:14 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/bnks_project/BnksProjectApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.bnks_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnksProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BnksProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/bnks_project/BnksProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.bnks_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnksProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
